e26f09c1b2b624931a8d4cce11299f6ac43ba36d
Luca Mella <lucam.ko@gmail.com>
2013-10-27 20:22:18 +0100

    train script

---
+++ src/dataset/train_models.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DS_FOLDER=../dataset
+DS_CONFIG=default.conf
+FACEDETECTOR=../resources/haarcascade_frontalface_default.xml
+
+
+
+#
+# PHASE 1
+# Finding relevant features using AdaBoost.
+#
+
+echo "1.1) Cropping faces"
+python2 ./datasetCropFaces.py $DS_FOLDER $FACEDETECTOR
+echo "------------------------"
+
+echo "1.2.a) Calculating features using bank of Gabor magnitude filters"
+python2 ./datasetFeatures.py $DS_FOLDER
+echo "------------------------"
+
+echo "1.2.b) Preparing CSV file containing training data"
+python2 ./datasetPrepTrain.py $DS_FOLDER 
+echo "------------------------"
+
+echo "1.3) Training with AdaBoost and selecting relevant features"
+python2 ./datasetTrain.py $DS_FOLDER 
+echo "------------------------"
+
+
+#
+# PHASE 2
+# Using relevant features for SVM training.
+#
+
+
+
