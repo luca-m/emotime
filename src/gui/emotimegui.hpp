0046ca5ad8812afafffa80945af257f747cfbaec
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-12-31 15:24:30 +0100

    Emotime GUI (webcam and Ada or SVM)

---
+++ src/gui/emotimegui.hpp
@@ -0,0 +1,53 @@
+/**
+ * @class    EmotimeGUI
+ * @author   Daniele Bellavista (daniele.bellavista@studio.unibo.it)
+ * @date    12/31/2013 10:53:55 AM
+ *
+ * @brief   Emotime GUI: capture from a webcam and predict the emotion
+ *
+ * @details
+ *
+ */
+
+#include "agui.hpp"
+#include "capture.hpp"
+
+#ifndef  __EMOTIME_GUI_INC__
+#define  __EMOTIME_GUI_INC__
+
+namespace emotime {
+
+  template < class T > class EmotimeGUI : public AGui<T> {
+    public:
+
+      EmotimeGUI(FacePreProcessor * fp, EmoDetector<T> * detect, int fps) :
+        AGui<T>(new Webcam(true), fp, detect, fps) {
+
+      }
+
+      ~EmotimeGUI() {
+        delete this->capture;
+      }
+
+    protected:
+
+      bool newFrame(Mat& frame, pair<Emotion, float> prediction) {
+        Mat copy;
+        frame.copyTo(copy);
+        stringstream ss;
+        ss << "Emotion=" << emotionStrings(prediction.first) << ", Score=" << prediction.second;
+        string osd = ss.str();
+
+        cv::putText(frame, osd.c_str(), Point(80,60), FONT_HERSHEY_SIMPLEX, 0.7, Scalar::all(0));
+        // QT only
+        //displayOverlay(mainWinTitle.c_str(), osd.c_str(), 2000);
+        return true;
+      }
+
+    private:
+
+  }; /* -----  end of class EmotimeGUI  ----- */
+
+}
+
+#endif   /* ----- #ifndef __EMOTIME_GUI_INC__  ----- */
