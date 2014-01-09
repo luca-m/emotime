527d6e0551c4d1fc6a21c4786b97a15e1fb2c064
Luca Mella <lucam.ko@gmail.com>
2014-01-09 14:57:05 +0100

    enhanched cli options and params

--- src/dataset/train_models.sh
+++ src/dataset/train_models.sh
@@ -1,73 +1,107 @@
 #!/bin/bash
+##
+## Description:
+##
+##    Train script
+##
+## Usage:
+##
+##    train_models.sh OPTIONS MODE PREPTRAINMODE DATASET
+##
+## Parameters:
+##    
+##    MODE            ada, svm
+##    PREPTRAINMODE   1vs1, 1vsall, 1vsallext
+##    DATASET         Dataset folder
+##
+## Options:
+##    
+##    -h, --help      This help message
+##    --no-eye        Do not perform eye-based face rotation correction
+##
+## Example:
+##    
+##    ./train_models.sh -h
+##
 
+# ----------------
+# UTILITY FUNCTIONS
+# ----------------
+
+echoerr() { 
+  echo "$@" 1>&2; 
+}
+usage() {
+  [ "$*" ] && echoerr "$0: $*"
+  sed -n '/^##/,/^$/s/^## \{0,1\}//p' "$0" 1>&2
+  exit 2
+}
+
+# default params
 DS_FOLDER=../dataset
 DS_CONFIG=default.cfg
-
 MODE=adaboost
 PREPTRAINMODE=1vsAll
 EYE="--eye-correction"
 
-if [[ $# -gt 0 ]] ; then
-  if [[ $1 == "svm" ]]; then
-    MODE=svm
-    PREPTRAINMODE=1vsAllExt
-    EYE=""
-  elif [[ $1 == "adaboost" ]]; then
-    MODE=adaboost
-  fi
-  if [[ $# -gt 1 ]] ; then
-    case ${2,,} in
-      "1vsall")
-        PREPTRAINMODE=1vsAll
-        ;;
-      "1vsallext")
-        PREPTRAINMODE=1vsAllExt
-        ;;
-      "1vs1")
-        PREPTRAINMODE=1vs1
-        ;;
-      *)
-        ;;
-    esac
-    if [[ $# -gt 2 ]] ; then
-      if [[ ${3} == "eye" ]]; then
-        EYE="--eye-correction"
-      else
-        EYE=""
-      fi
-    fi
-  fi
+# ----------------
+# MAIN
+# ----------------
+
+# Options parsing
+while [ $# -gt 0 ]; do
+  case $1 in
+    (-h|--help) usage 2>&1;;
+    (--no-eye) EYE='';;
+    (--) shift; break;;
+    (-*) usage "$1: unknown option";;
+    (*) break;;
+  esac
+done
+
+if [[ $# -lt 3 ]] ; then
+    echoerr "ERR: Missing mandatory parameter"
+    exit -1
 fi
 
-#
-# PHASE 1
-# Finding relevant features using AdaBoost.
-#
+# Parameter parsing
+if [[ $1 == "svm" ]]; then
+  MODE='svm'
+elif [[ $1 == "adaboost" ]]; then
+  MODE='adaboost'
+fi
 
-echo "1.1) Cropping faces"
+case ${2,,} in
+  "1vsall")
+    PREPTRAINMODE='1vsAll'
+    ;;
+  "1vsallext")
+    PREPTRAINMODE='1vsAllExt'
+    ;;
+  "1vs1")
+    PREPTRAINMODE='1vs1'
+    ;;
+  *)
+    ;;
+esac
+
+DS_FOLDER="$3"
+
+echo "1) Cropping faces:"
 python2 ./datasetCropFaces.py $DS_FOLDER $EYE
 echo "------------------------"
-
-echo "1.2.a) Calculating features using bank of Gabor magnitude filters"
+echo "2) Calculating features using bank of Gabor magnitude filters:"
 python2 ./datasetFeatures.py $DS_FOLDER
 echo "------------------------"
-
-echo "1.2.b) Preparing CSV file containing training data"
+# Non dataset dependent standard cleanup
+rm $DS_FOLDER/training/*.csv
+echo "3) Preparing CSV file containing training data:"
 python2 ./datasetPrepTrain.py $DS_FOLDER --mode $PREPTRAINMODE
 echo "------------------------"
-
-echo "1.3) Training with $MODE and selecting relevant features"
+echo "4) Training with $MODE and selecting relevant features:"
 python2 ./datasetTrain.py $DS_FOLDER --mode $MODE
 echo "------------------------"
-
-echo "1.4) Verifying the prediction of $MODE"
+echo "5) Verifying the prediction of $MODE:"
 python2 ./datasetVerifyPrediction.py --mode $MODE $DS_FOLDER $EYE
 echo "------------------------"
-
-#
-# PHASE 2
-# Using relevant features for SVM training.
-#
-
-
 
