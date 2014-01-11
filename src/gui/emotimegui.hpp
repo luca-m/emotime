2687429224fbc32a82901c50620edca61e256b00
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2014-01-11 11:30:59 +0100

    More visible text

--- src/gui/emotimegui.hpp
+++ src/gui/emotimegui.hpp
@@ -34,11 +34,14 @@ namespace emotime {
       bool newFrame(Mat& frame, pair<Emotion, float> prediction) {
         Mat copy;
         frame.copyTo(copy);
-        stringstream ss;
-        ss << "Emotion=" << emotionStrings(prediction.first) << ", Score=" << prediction.second;
+        stringstream ss, ss2;
+        ss << "Emotion: " << emotionStrings(prediction.first);
+        ss2 << "Score: " << prediction.second;
         string osd = ss.str();
+        string osd2 = ss2.str();
 
-        cv::putText(frame, osd.c_str(), Point(80,60), FONT_HERSHEY_SIMPLEX, 0.7, Scalar::all(0));
+        cv::putText(frame, osd.c_str(), Point(20,60), FONT_HERSHEY_SIMPLEX, 0.7, Scalar::all(255));
+        cv::putText(frame, osd2.c_str(), Point(20,100), FONT_HERSHEY_SIMPLEX, 0.7, Scalar::all(255));
         // QT only
         //displayOverlay(mainWinTitle.c_str(), osd.c_str(), 2000);
         return true;
